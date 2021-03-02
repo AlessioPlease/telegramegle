0f4eb2fb1a1c46ce20c81daeac0a24216235cbf5
Alessio <squitt00@yahoo.it>
2021-03-02 18:12:59 +0100

    + added telegramSetup.py to retrieve user ID in order for the Telegram bot to function

---
+++ telegramegle/telegramSetup.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+updatesUrl = 'https://api.telegram.org/bot<YOUR BOT TOKEN>/getUpdates'
+userId = ''
+
+data = {
+    'offset': ''
+}
+
+answer = requests.post(updatesUrl, data=data)
+responseData = json.loads(answer.text)
+if responseData['ok']:
+    if responseData['result']:
+        for result in responseData['result']:
+            userId = result['message']['from']['id']
+else:
+    print("COULD NOT RETRIEVE ID")
+
+print(userId)
